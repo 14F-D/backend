e0a83754c60937a13ddd185126afa2e2b7e2e386
Kincses-Tamas <kincses.tamas@students.jedlik.eu>
2023-04-17 11:17:41 +0200

    date

--- controllers/expenses.controller.js
+++ controllers/expenses.controller.js
@@ -38,7 +38,7 @@ const expenses = {
     create(req, res) {
         const newExpense = {
             amount: req.body.amount,
-            date: new Date(),
+            date: new Date().toLocaleString(),
             userId: req.session.user.id
         };
         const sql = 'insert into expenses set ?';
